ea5aa62bc6f8b71f5485245d82e18c9f57d7a03f
Your Name <you@example.com>
2020-02-17 07:46:14 +0000

    complate

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '47457ba6f984713a68442cd877fd50eef088c32d864a8249d7f9333439273bd27643b3a71d8ffa368d0b8c6a02accb4269bed18f9ebf6ece11a19ea777271a34'
