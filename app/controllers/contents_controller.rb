e2cad3128fbd109eb44effc614d793061c660cfa
Your Name <you@example.com>
2020-02-23 02:32:21 +0000

    better-errors-dne

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -21,6 +21,7 @@ class ContentsController < ApplicationController
         else
             @content = Content.none
         end
+        @user = User.find(params[:id])
     end
     def show
         @comment = Comment.new
