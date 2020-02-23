3f1b5788afb1e99f520c1ed04a7d03668a93bab0
Your Name <you@example.com>
2020-02-23 07:56:48 +0000

    done

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,9 @@ Bundler.require(*Rails.groups)
 module Qiita
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :ja    
+    config.load_defaults 5.2
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
