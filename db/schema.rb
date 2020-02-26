b26aaa2cc5f56efa59cadc43c0005f0967d0d9f5
Your Name <you@example.com>
2020-02-26 07:52:54 +0000

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_014257) do
+ActiveRecord::Schema.define(version: 2020_02_26_075143) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
@@ -91,6 +91,8 @@ ActiveRecord::Schema.define(version: 2020_02_25_014257) do
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
